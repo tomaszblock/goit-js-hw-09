339d6bfe5b978b0fdeb60563d931388750aab9de
tomaszblock <127046228+tomaszblock@users.noreply.github.com>
2024-02-10 22:52:24 +0000

    Deploying to gh-pages from @ tomaszblock/goit-js-hw-09@43538acfa126c1afd650ac1a7f2d7cdb3b8327f4 🚀

---
+++ 03-promises.ccd7fecb.js
@@ -0,0 +1,2 @@
+document.querySelector('[name="delay"]'),document.querySelector('[name="step"]'),document.querySelector('[name="amount"]');document.querySelector(".form").addEventListener("submit",void(Math.random()>.3?console.log("tak"):console.log("nie")));
+//# sourceMappingURL=03-promises.ccd7fecb.js.map
---
+++ 03-promises.cf9a84e9.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector('[name="delay"]'),document.querySelector('[name="step"]'),document.querySelector('[name="amount"]');document.querySelector(".form").addEventListener("submit",void(Math.random()>.3?console.log("tak"):console.log("nie")))}();
+//# sourceMappingURL=03-promises.cf9a84e9.js.map
