ca9b2a08591a164a533085e3b4ca71c3a5452c5e
tomaszblock <127046228+tomaszblock@users.noreply.github.com>
2024-02-12 18:55:38 +0100

    ad3 trying

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -3,15 +3,27 @@ const stepInput = document.querySelector('[name="step"]');
 const amountInput = document.querySelector('[name="amount"]');
 const form = document.querySelector('.form');
 
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-    console.log('tak');
-  } else {
-    // Reject
-    console.log('nie');
+function createPromise(event, position, delay) {
+  event.preventDefault();
+  delay = parseInt(delayInput.value);
+  let step = parseInt(stepInput.value);
+  let amount = parseInt(amountInput.value);
+  let result = 0;
+  event.preventDefault();
+  for (let i = 1; i <= amount; i++) {
+    new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const shouldResolve = Math.random() > 0.3;
+        if (shouldResolve) {
+          // Fulfill
+          console.log('tak');
+        } else {
+          // Reject
+          console.log('nie');
+        }
+      }, result);
+    });
   }
 }
 
-form.addEventListener('submit', createPromise());
+form.addEventListener('submit', createPromise);
