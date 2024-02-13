5629f648234809ee3188aeea3ae2392f05379741
tomaszblock <127046228+tomaszblock@users.noreply.github.com>
2024-02-13 14:52:57 +0100

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -3,27 +3,55 @@ const stepInput = document.querySelector('[name="step"]');
 const amountInput = document.querySelector('[name="amount"]');
 const form = document.querySelector('.form');
 
-function createPromise(event, position, delay) {
+function createPromise(event) {
   event.preventDefault();
-  delay = parseInt(delayInput.value);
-  let step = parseInt(stepInput.value);
-  let amount = parseInt(amountInput.value);
-  let result = 0;
-  event.preventDefault();
+  const delay = parseInt(delayInput.value);
+  const step = parseInt(stepInput.value);
+  const amount = parseInt(amountInput.value);
+
+  let currentDelay = delay; // początkowe opóźnienie
+
   for (let i = 1; i <= amount; i++) {
-    new Promise((resolve, reject) => {
-      setTimeout(() => {
+    setTimeout(() => {
+      new Promise((resolve, reject) => {
         const shouldResolve = Math.random() > 0.3;
         if (shouldResolve) {
-          // Fulfill
-          console.log('tak');
+          resolve();
         } else {
-          // Reject
-          console.log('nie');
+          reject();
         }
-      }, result);
-    });
+      })
+        .then(result => {
+          console.log(`✅ Fulfilled promise ${i} in ${currentDelay}ms`);
+          currentDelay = currentDelay + step;
+        })
+        .catch(error => {
+          console.log(`❌ Rejected promise ${i} in ${currentDelay}ms`);
+          currentDelay = currentDelay + step;
+        });
+    }, currentDelay);
   }
 }
+
+// function createPromise(event, position, delay) {
+//   event.preventDefault();
+//   delay = parseInt(delayInput.value);
+//   let step = parseInt(stepInput.value);
+//   let amount = parseInt(amountInput.value);
+//   let result = 0;
+//   for (let i = 1; i <= amount; i++) {
+//     new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         const shouldResolve = Math.random() > 0.3;
+//         if (shouldResolve) {
+//           resolve(console.log('tak'));
+//         } else {
+//           // Reject
+//           reject(console.log('nie'));
+//         }
+//       }, delay);
+//     });
+//   }
+// }
 
 form.addEventListener('submit', createPromise);
